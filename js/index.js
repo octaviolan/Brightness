ceefa14723d005f3297eb55739c546eb0c602fa3
octaviolan <octavio.olan25@gmail.com>
2022-11-06 12:28:38 -0600

    version mobile

--- js/index.js
+++ js/index.js
@@ -4,21 +4,19 @@ const inputRange = document.getElementById('range');
 inputRange.addEventListener('input', ()=> {
   
   const value = inputRange.value;
+  const percentage = (value / inputRange.max) * 100;
+  const linearGradient = `linear-gradient(90deg, var(--primary) ${percentage}%, rgba(48, 127, 226, 0.3) ${percentage}%)`;
   
-  console.log(value);
-  
+  inputRange.style.background = linearGradient;
+
   if(value == 1) {
     body.style.backgroundColor = '#221F32';
-    inputRange.style.background = 'linear-gradient(90deg, var(--primary) 33.3%, rgba(48, 127, 226, 0.3) 33.3%)';
   } else if (value == 2) {
     body.style.backgroundColor = '#322F46';
-    inputRange.style.background = 'linear-gradient(90deg, var(--primary) 66.6%, rgba(48, 127, 226, 0.3) 66.6%)';
   } else if(value == 3) {
     body.style.backgroundColor = '#443F72';
-    inputRange.style.background = 'linear-gradient(90deg, var(--primary) 99.9%, rgba(48, 127, 226, 0.3) 99.9%)';
   } else {
     body.style.backgroundColor = '#0F0E17';
-    inputRange.style.background = 'linear-gradient(90deg, var(--primary) 0%, rgba(48, 127, 226, 0.3) 0%)';
       }
 })
 
